d208bea433b64c28ba16384e429534be0e66a915
Justin Lee <justin.lee@ubc.ca>
2017-02-15 13:17:56 -0800

    Finished implementing core functions

--- src/core.js
+++ src/core.js
@@ -2,8 +2,26 @@ import {List, Map} from 'immutable';
 
 export const setEntries = (state, entries) => state.set('entries', List(entries));
 
-export const next = (state) => {
-    const entries = state.get('entries');
+const getWinners = vote => {
+    if (!vote) return [];
+    const [a, b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0);
+    const bVotes = vote.getIn(['tally', b], 0);
+    if (aVotes > bVotes) return [a];
+    else if (aVotes < bVotes) return [b];
+    else return [a,b];
+}
+
+export const next = state => {
+    const entries = state.get('entries')
+                         .concat(getWinners(state.get('vote')));
+
+    if (entries.size === 1) {
+        return state.remove('vote')
+                    .remove('entries')
+                    .set('winner', entries.first());
+    }
+
     return state.merge({
         vote: Map({pair: entries.take(2)}),
         entries: entries.skip(2)
